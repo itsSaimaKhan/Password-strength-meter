50a3c7045d9ccd324933354e2ef449de07bbc1ed
itssaimaKhan <samiakhan8754@gmail.com>
2025-04-18 23:15:15 +0500

    first commit

---
+++ psm.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import re
+
+st.set_page_config(page_title="Password Strength Checker", page_icon="🔒")
+
+st.title("🔐Password Strength Checker")
+st.markdown("""
+"##welcome to the ultimate password strength checker!👋
+use this simple tool to check the strength of your password and get suggestions on how to make it stronger.
+            we will give you helpful tips to create a **Strong Password** 🔒""")
